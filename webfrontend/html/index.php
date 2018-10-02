f24ee4f59aed75076437662d85f494d02d9672fd
Wörsty <git@loxberry.woerstenfeld.de>
2018-10-02 22:09:25 +0200

    Changed from file_get_contents to cURL to enable redirects
    Fixes #1 (URL with redirect)

--- webfrontend/html/index.php
+++ webfrontend/html/index.php
@@ -1,12 +1,27 @@
 <?php 
+error_reporting(~E_ALL & ~E_STRICT);     // Keine Fehler reporten (auch nicht E_STRICT)
+ini_set("display_errors", false);        // Fehler nicht direkt via PHP ausgeben
 if (isset($_REQUEST['url']))
 {
-	$ctx = stream_context_create(array( 'http' => array( 'timeout' => 3 ) ) ); 
-  $content = file_get_contents("https://".substr($_REQUEST['url'],8), 0, $ctx);
-	if (isset($content) && strlen($content) > 1) 
+    $curl = curl_init() or die("cURL init error");
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPAUTH, constant(CURLAUTH_ANY));
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_URL, "https://".substr($_REQUEST['url'],8));
+	$out = curl_exec($curl);
+	curl_close($curl); 
+	if ($out === false )
 	{
-		echo $content;
-		exit;
+		die("cURL read error");
+	}	
+	else
+	{
+		echo $out;
 	}
+	exit;
 }
-echo "Error - URL correct?";
+else
+{
+	echo "Error - URL correct?";
+}
